b325c00d785978e95c99061942ebe2197cbf0e20
Carla Stickler <carlastickler@gmail.com>
2019-04-18 21:45:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,9 +58,11 @@ def monopoly_with_fourth_tier
 	         "mortgage_value": "$200"
 	       },
 	       :b_and_o_railroad => {
-	         "mortgage_value": ""
+	         "mortgage_value": "$400"
 	       },
-	       :shortline => {},
+	       :shortline => {
+	         "mortgage_value": "$800"
+	       },
 	     }, 
 	     :rent_in_dollars => {
 	       one_piece_owned: 25,
